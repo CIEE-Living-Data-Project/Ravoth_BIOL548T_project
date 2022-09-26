e027c0040d2b75e5c933b392d2f00413752162e5
sarah-ravioli <sarah.ravoth@gmail.com>
2022-09-25 22:06:30 -0600

    wrote the tidied version into a csv file to use for figures script

--- scripts/figure_script.R
+++ scripts/figure_script.R
@@ -11,24 +11,26 @@ groundhog.library(pkg = c("tidyverse", "ggplot2"), date="2022-09-14")
 
 ######################### LOAD IN DATA ######################### 
 
-### changed to tidied data!!!!!1
-
 # note: tried to make a relative file path but it wasn't working--i will figure this out for final submission
 getwd() # check
 setwd("/Users/sarahravoth/Desktop/biol548T//Ravoth_BIOL548T_project/data") # reset
 
-sal <-  read.csv("BrucePNP_RedBackedSal_2018_abun.csv",  # read in data 
-                 fileEncoding = "Latin1") # the second line is french column name with accent characters, so need this to read in the accents, or can't run/error 
+sal.t <-  read.csv("tidied_BrucePNP_RedBackedSal_2018_abun.csv")  # read in data 
 
 
 ######################### MAKE THE FIGURE ######################### 
 
+sal.t %>%
+  filter(count!="NA", Soil.Temperature...C.!="N/A") %>%   # filtered out NAs
+  ggplot(aes(x=as.numeric(Soil.Temperature...C.), y=count, col=Morph)) +  # changed the class for soil temperature from character to numeric (or else doesn't plot properly)--this was plotted on the x axis, abundance plotted on y axis, and points coloured by redback salamander morph
+  geom_point(position = "jitter") +  # jitter points so it gives a better image of underlying data
+  geom_smooth(method="lm") + # fit lines to data & specify fitting method (lm = linear model)
+  theme_classic() + # change plotting theme
+  labs(x="Soil temperature (celsuis)", y="Abundance",  # change axis labels
+       caption="Figure 1. Affect of soil temperature on abundance of redback salamanders in Bruce Peninsula National Park using a linear model") +  # name the figure in a caption
+  scale_color_manual(values=c("#7A7A7A", "#CB0000")) # change colours to match morph
 
 
-
-
-
-
-
+# the end! 
 
 
