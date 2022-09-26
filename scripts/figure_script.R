6f55409f8dc0ba93d115bc8862362591e008d091
sarah-ravioli <sarah.ravoth@gmail.com>
2022-09-25 22:01:31 -0600

    - made a script for tidying data - made a script for plotting data - made 2 branches for these processes

--- scripts/figure_script.R
+++ scripts/figure_script.R
@@ -1,5 +1,5 @@
 #### REDBACK SALAMANDER ABUNDANCE FIGURES (BIOL548T)
-#### DATE CREATED: 16 SEPTEMBER 2022
+#### DATE CREATED: 25 SEPTEMBER 2022
 #### BY: SARAH RAVOTH
 
 ######################### ENABLE GROUNDHOG ######################### 
@@ -7,23 +7,28 @@ library(groundhog)
 groundhog.library(pkg = c("tidyverse", "ggplot2"), date="2022-09-14")
 
 
+
+
 ######################### LOAD IN DATA ######################### 
+
+### changed to tidied data!!!!!1
+
+# note: tried to make a relative file path but it wasn't working--i will figure this out for final submission
 getwd() # check
-setwd("/Users/sarahravoth/Desktop/biol548T/Ravoth_BIOL548T_project/data") # reset
+setwd("/Users/sarahravoth/Desktop/biol548T//Ravoth_BIOL548T_project/data") # reset
 
 sal <-  read.csv("BrucePNP_RedBackedSal_2018_abun.csv",  # read in data 
                  fileEncoding = "Latin1") # the second line is french column name with accent characters, so need this to read in the accents, or can't run/error 
 
 
+######################### MAKE THE FIGURE ######################### 
 
-######################### TIDY DATA ######################### 
-# tidying whole dataset would be a project itself, so i'll just tidy what's necessary for the figure
 
-# take a look at data
-head(sal)
-str(sal)
-summary(sal)
 
-# remove second row (french colnames)
+
+
+
+
+
 
 
