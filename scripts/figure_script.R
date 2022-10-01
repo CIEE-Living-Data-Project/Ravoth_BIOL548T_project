45362ea39ff2ccf9420711d034a8a8ee20aedbbc
sarah-ravioli <sarah.ravoth@gmail.com>
2022-10-01 15:21:55 -0600

    - changed line saving figure from an absolute to relative path - fixed error on line 21 that was incorrectly keeping NA values for soil temp

--- scripts/figure_script.R
+++ scripts/figure_script.R
@@ -8,7 +8,6 @@ groundhog.library(pkg = c("tidyverse", "ggplot2"), date="2022-09-14")
 
 
 
-
 ######################### LOAD IN DATA ######################### 
 # record the url to download data from my github repo
 url_sal.t <-"https://raw.githubusercontent.com/CIEE-Living-Data-Project/Ravoth_BIOL548T_project/main/data/tidied_BrucePNP_RedBackedSal_2018_abun.csv"
@@ -19,8 +18,8 @@ sal.t <-read_csv(url_sal.t)
 
 ######################### MAKE THE FIGURE ######################### 
 sal.fig <- sal.t %>%
-  filter(count!="NA", `Soil Temperature (°C)`=="N/A") %>%   # filtered out NAs
-  ggplot(aes(x=as.numeric(`Soil Temperature (°C)`), y=count, col=Morph)) +  # changed the class for soil temperature from character to numeric (or else doesn't plot properly)--this was plotted on the x axis, abundance plotted on y axis, and points coloured by redback salamander morph
+  filter(count!=is.na(count), `Soil Temperature (°C)`!="N/A") %>%   # filtered out NAs
+  ggplot(aes(x=as.numeric(`Soil Temperature (°C)`), y=as.numeric(count), col=Morph)) +  # changed the class for soil temperature & abundance count from character to numeric (or else doesn't plot properly)--this was plotted on the x axis, abundance plotted on y axis, and points coloured by redback salamander morph
   geom_point(position = "jitter") +  # jitter points so it gives a better image of underlying data
   geom_smooth(method="lm") + # fit lines to data & specify fitting method (lm = linear model)
   theme_classic() + # change plotting theme
@@ -30,11 +29,9 @@ sal.fig <- sal.t %>%
 
 ######################### SAVE THE FIGURE ######################### 
 # saving figure to the outputs folder
-# will change this to a relative file path!
-setwd("/Users/sarahravoth/Desktop/biol548T//Ravoth_BIOL548T_project/outputs")
 
+setwd("./outputs")
 png(paste0("SoilTemp_RedBackedSalMorph_AbundanceFig_",Sys.Date(),".png" )) # set document to write plots to
-sal.fig
 dev.off()
     
 
